76327376305e43bff1cb6c2006b50c5638d0a90c
Nishad Gothoskar <nishadgothoskar@gmail.com>
2018-09-03 11:43:17 -0400

    experiment

--- fast_mio.py
+++ fast_mio.py
@@ -34,26 +34,33 @@ def sample_function(x_range, N=100, seed=4):
     def f(x): return m.predict(x)[0]
     return f,m,x
 
-for i in range(20):
+
+data = []
+
+for i in range(5):
     print i
     x_range = np.array([[-5.0,-5.0,-5.0], [5.0,5.0,5.0]])
     f,m,x = sample_function(x_range)
     x = np.random.uniform(x_range[0], x_range[1], (1, DIM))
     y = f(x)
     gp = GPy.models.GPRegression(x, y, GPy.kern.src.rbf.RBF(input_dim=DIM,lengthscale=LSCALE),noise_var=0.0)
-    gps = [gp]
     b = np.zeros(SDIM)
-    B = I = np.identity(SDIM)
-    gps = opt.OPT(f,gp,b,B,SDIM,ADIM,50,ABOUNDS,SBOUNDS,BETA,LSCALE)
+    B = np.identity(SDIM)*0.5
+    gps = opt.OPT(f,gp,b,B,SDIM,ADIM,80,ABOUNDS,SBOUNDS,BETA,LSCALE)
 
     actions = np.linspace(-6.0,6.0,1000)
     mus = np.array([])
-    preds = np.array([])
+    intervals = [10,20,30,40,50,60,70,79]
+    maxs = []
     for a in actions:
         mus = np.append(mus,opt.integrate_dim(m,SDIM, a, b,B)[0])
-        preds = np.append(preds,opt.integrate_dim(gps[-1],SDIM, a, b,B)[0])
-    print "MODEL " + str((actions[mus.argmax()], mus.max()))
-    print "GP " + str((actions[preds.argmax()], preds.max()))
+    for interv in intervals:
+        preds = np.array([])
+        for a in actions:
+            preds = np.append(preds,opt.integrate_dim(gps[interv],SDIM, a, b,B)[0])
+        maxs.append((actions[preds.argmax()], preds.max()))
+    data.append((actions[mus.argmax()], mus.max(),maxs))
+    print data
 
 
 
